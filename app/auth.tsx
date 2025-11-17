d90522ee254cccf53eb938afb8769a3051648fe4
Selva Mareeswaran <selvamareeswaranb@gmail.com>
2025-11-17 18:23:56 +0530

    feat(auth): implement Yup validation and react-hook-form for authentication form

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,14 +1,52 @@
 import { useState } from "react";
 import { KeyboardAvoidingView, Platform, View, StyleSheet } from "react-native";
-
 import { Button, Text, TextInput } from "react-native-paper";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm, Controller } from "react-hook-form";
+
+//yup validation schema
+const schema = yup.object().shape({
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
+    .matches(/[a-z]+/, "Password must contain at least one lowercase letter")
+    .matches(/[A-Z]+/, "Password must contain at least one uppercase letter")
+    .matches(/\d+/, "Password must contain at least one number")
+    .matches(
+      /[@$!%*#?&]+/,
+      "Password must contain at least one special character"
+    ),
+});
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
 
 export default function AuthScreen() {
   const [isSignUp, setIsSignUp] = useState(false);
 
+const {
+  control,
+  handleSubmit,
+  formState: { errors, submitCount },
+} = useForm<LoginForm>({
+  resolver: yupResolver(schema),
+  mode: "onSubmit",
+  reValidateMode: "onChange",
+});
+   
+  const onSubmit = async (data: LoginForm) => {
+    console.log("Form Data:", data);
+  };
   const handleSwitchMode = () => {
     setIsSignUp((prev) => !prev);
   };
+    console.log("Form Data:",errors);
+  const handleAuth = async () => {};
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -18,23 +56,49 @@ export default function AuthScreen() {
         <Text style={styles.title} variant="headlineMedium">
           {isSignUp ? "Create Account" : "Welcome Back"}
         </Text>
-        <TextInput
-          autoCapitalize="none"
-          keyboardType="email-address"
-          placeholder="example@gmail.com"
-          label={"email"}
-          mode="outlined"
-          style={styles.input}
+
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              value={value}
+              autoCapitalize="none"
+              keyboardType="email-address"
+              label={"Email"}
+              mode="outlined"
+              style={styles.input}
+              onChangeText={onChange}
+            />
+          )}
+        />
+        {errors.email && (
+          <Text style={styles.error}>{errors.email.message}</Text>
+        )}
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              autoCapitalize="none"
+              keyboardType="email-address"
+              label={"password"}
+              mode="outlined"
+              style={styles.input}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
         />
 
-        <TextInput
-          autoCapitalize="none"
-          keyboardType="email-address"
-          label={"password"}
-          mode="outlined"
-          style={styles.input}
-        />
-        <Button mode="contained" style={styles.button}>
+        {errors.password && (
+          <Text style={styles.error}>{errors.password.message}</Text>
+        )}
+        <Button
+          mode="contained"
+          style={styles.button}
+          onPress={handleSubmit(onSubmit)}
+        >
           {isSignUp ? "Sign Up" : "Sign In"}
         </Button>
 
@@ -67,12 +131,17 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   input: {
-    marginBottom: 16,
+    marginBottom: 12,
   },
   button: {
     marginTop: 8,
   },
   switchButton: {
     marginTop: 16,
+  },
+  error: {
+    color: "red",
+    marginBottom: 8,
+    fontSize: 12,
   },
 });
